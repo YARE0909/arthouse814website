f3522280ef52041e3bfd85a99ffca30e44d7a322
YARE0909 <pradyumnad092004@gmail.com>
2024-06-14 15:52:55 +0530

    chore: Add WhatsApp contact button to layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
@@ -8,11 +9,17 @@ export default function Home({ children }: { children: React.ReactNode }) {
       <div className="w-full">
         <Navbar />
       </div>
-      <div className="w-full flex-grow flex flex-col items-center justify-center bg-white shadow-2xl rounded-lg mt-6 p-8">
+      <div className="w-full flex-grow flex flex-col items-center justify-center bg-white shadow-2xl rounded-lg mt-6 lg:p-8">
         {children}
       </div>
       <div className="w-full mt-auto">
         <Footer />
+      </div>
+      <div className="fixed bg-green-500 w-32 rounded-lg bottom-10 right-0 p-2">
+        <a className="w-full flex gap-2 items-center" target="_blank" href="https://wa.me/919972658401">
+          <FaWhatsapp  className="text-white text-3xl cursor-pointer" />
+          <h1 className="text-white font-medium text-xs">Contact Us</h1>
+        </a>
       </div>
     </div>
   );
