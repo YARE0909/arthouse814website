781f648123d8ac1cabe6823d1921d39346f1ed35
YARE0909 <pradyumnad092004@gmail.com>
2024-07-03 20:58:13 +0530

    feat: Add page title to Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { FaWhatsapp } from "react-icons/fa";
+import Head from "next/head";
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-fixed bg-cover bg-no-repeat lg:px-52 gap-5 bg-gradient-to-r from-blue-200 via-teal-300 to-green-200">
+      <Head>
+        <title>Arthouse 814</title>
+      </Head>
       <div className="w-full">
         <Navbar />
       </div>
