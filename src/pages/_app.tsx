85586b05db6fbdada1d20f07503422f63defaa44
YARE0909 <pradyumnad092004@gmail.com>
2024-07-04 15:48:39 +0530

    feat: Update npm dependencies for @vercel/analytics to version 1.3.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import localFont from "@next/font/local";
-
-const jokerMan = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Jokerman.otf",
-      weight: "400",
-    },
-  ],
-  variable: "--font-jokerman",
-});
-
-const luciaCalligraphy = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Lucida Calligraphy Font.ttf",
-      weight: "400",
-    },
-  ],
-  variable: "--font-lucia-calligraphy",
-});
-
-// const elephant = localFont({
-//   src: [
-//     {
-//       path: "../../public/fonts/ELEPHNT.TTF",
-//       weight: "400",
-//     },
-//   ],
-//   variable: "--font-elephant",
-// });
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
